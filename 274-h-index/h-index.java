2a3c40930b83e09c4c9d27973029bb70c7256bbb
Ashish Sharma <86611394+ashishh-81@users.noreply.github.com>
2026-07-11 09:15:04 +0530

    Time: 5 ms (71.88%) | Memory: 43.5 MB (60.49%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int count=0;
+        
+        for(int i=0;i<citations.length;i++){
+            
+            if(citations[i]>= citations.length-i){
+                count++;
+            }
+            else{
+                count = count +0;
+            }
+        }
+        return count;
+    }
+}
